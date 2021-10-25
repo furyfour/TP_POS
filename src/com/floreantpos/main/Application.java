c609b776380eab0f45111ebb75db80b536516030
Jason Denett <jdenett@gmu.edu>
2021-10-24 23:08:49 -0400

    Removed Old Derby Database

--- src/com/floreantpos/main/Application.java
+++ src/com/floreantpos/main/Application.java
@@ -561,7 +561,7 @@ public class Application {
 	}
 
 	public static String getTitle() {
-		return "Floreant POS - Version " + VERSION; //$NON-NLS-1$
+		return "Tony's POS - Version " + VERSION; //$NON-NLS-1$
 	}
 
 	public static ImageIcon getApplicationIcon() {
